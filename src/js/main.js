063b2fe2f1828b5c5417c6c5aaa6ccc5cda3b09b
Jason Graziano <mrehawk82@gmail.com>
2019-01-22 16:53:27 -0500

    new things new problems

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,19 @@
-console.log("mainJS");
+var ui;
+
+ui = {
+    init: () => {
+    	ui.myLoad();
+    },
+    myLoad: () => {
+        var vContain = createEle("div");
+
+        vContain.innerHTML = "done";
+
+        dvContain.appendChild(vContain);
+
+    }
+};
+
+window.onload = () => {
+    ui.init();
+};
