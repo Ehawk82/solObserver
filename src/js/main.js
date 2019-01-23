aea28d56fdf08254eb438fff24da6be42b867dad
Jason Graziano <mrehawk82@gmail.com>
2019-01-23 13:26:38 -0500

    update for JS and UI

--- src/js/main.js
+++ src/js/main.js
@@ -1,30 +1,103 @@
-var ui;
+var ui, userdata;
+
+userdata = {
+	volts: 0,
+	amp: 0
+}
 
 ui = {
     init: () => {
-    	ui.myLoad();
-    },
-    myLoad: () => {
-        var vContain = createEle("div"),
-            vIn = createEle("input");
+    	localStorage.clear();
+    	var uData = localStorage.getItem("userdata");
         
+    	if (!uData || uData === null) {
+            localStorage.setItem("userdata", JSON.stringify(userdata));
+    	};
+    	uData = localStorage.getItem("userdata");
+    	if (uData) {
+            var uuu = JSON.parse(uData);
+    	};
+
+    	ui.myLoad(uuu);
+    },
+    myLoad: (uuu) => {
+        var vContain = createEle("div"),
+            wimg = createEle("img"),
+            aimg = createEle("img"),
+            vimg = createEle("img"),
+            wimgWrapper = createEle("div"),
+            aimgWrapper = createEle("div"),
+            vimgWrapper = createEle("div"),
+            vIn = createEle("input"),
+            aIn = createEle("input"),
+            wIn = createEle("input");
+
+        wIn.className = "ins";
+        wIn.type = "number";
+        wIn.placeholder = "0";
+        wIn.readOnly = true;
+
+        aIn.className = "ins";
+        aIn.type = "number";
+        aIn.placeholder = uuu.amp;
+        aIn.addEventListener("keyup", ui.ampIn(aIn, uuu, wIn));
+
         vIn.className = "ins";
         vIn.type = "number";
-        vIn.placeholder = "Enter Voltage";
-        vIn.addEventListener("keyup", ui.voltageIn(vIn), false);
+        vIn.placeholder = uuu.volts;
+        vIn.addEventListener("keyup", ui.voltageIn(vIn, uuu, wIn));
 
+        wimg.className = "imgs";
+        wimg.src = "src/assets/watt-image.png";
 
-        vContain.innerHTML = "voltage";
+        aimg.className = "imgs";
+        aimg.src = "src/assets/amp-image.png";
+
+        vimg.className = "imgs";
+        vimg.src = "src/assets/volt-image.png";
+
+        wimgWrapper.className = "imgWrapper";
+        wimgWrapper.appendChild(wimg);
+
+        aimgWrapper.className = "imgWrapper";
+        aimgWrapper.appendChild(aimg);
+
+        vimgWrapper.className = "imgWrapper";
+        vimgWrapper.appendChild(vimg);
+
         vContain.className = "containers";
+        
+        vContain.appendChild(vimgWrapper);
         vContain.appendChild(vIn);
-
+        vContain.appendChild(aimgWrapper);
+        vContain.appendChild(aIn);
+        vContain.appendChild(wimgWrapper);
+        vContain.appendChild(wIn);
 
         dvContain.appendChild(vContain);
     },
-    voltageIn: (vIn) => {
+    voltageIn: (vIn, uuu, wIn) => {
     	return () => {
-    		console.log(vIn.value);
+    		uuu.volts = vIn.value
+
+    		localStorage.setItem("userdata", JSON.stringify(uuu));
+    		ui.wattUpdate(uuu, wIn);
+
         }
+    },
+    ampIn: (aIn, uuu, wIn) => {
+    	return () => {
+    		uuu.amp = aIn.value
+
+    		localStorage.setItem("userdata", JSON.stringify(uuu));
+    		ui.wattUpdate(uuu, wIn);
+        }
+    },
+    wattUpdate: (uuu, wIn) => {
+
+            wIn.value = +uuu.volts * +uuu.amp;
+
+        
     }
 };
 
